9bfba33a2e79531ef5d8ffca96d3805816f3373c
김선광 <blueksg0307@gmail.com>
2019-05-21 01:56:24 +0900

    routes has been changed by sean

--- express-generator/routes/index.js
+++ express-generator/routes/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
-});
 
 router.get('/dashboard', function(req, res, next) {
   res.render('dashboard' );
@@ -12,42 +9,52 @@ router.get('/dashboard', function(req, res, next) {
 
 
 router.get('/404', function(req, res, next) {
-  res.render('404' );
+  res.render('./main/etc/404' );
 });
 
-
+// User 기능과 관련된 페이지 시작
 router.get('/login', function(req, res, next) {
-  res.render('login' );
+  res.render('./main/User/login' );
 });
-
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup' );
+  res.render('./main/User/signup' );
 });
 
 router.get('/forgotpassword', function(req, res, next) {
-  res.render('forgotpassword' );
-});
-
-router.get('/empty', function(req, res, next) {
-  res.render('empty' );
-});
-
-router.get('/logmanagement', function(req, res, next) {
-  res.render('logmanagement' );
-});
-
-router.get('/agentmanagement', function(req, res, next) {
-  res.render('agentmanagement' );
+  res.render('./main/User/forgotpassword' );
 });
 
 router.get('/changeinfo', function(req, res, next) {
-  res.render('changeinfo' );
+  res.render('./main/User/changeinfo' );
 });
 
-router.get('/grouppolicy', function(req, res, next) {
-  res.render('grouppolicy' );
-});
+// User 기능과 관련된 페이지 끝
+
+
+router.get('/agent', function(req, res, next) {
+  res.render('./main/Agent/agent');
+});// 에이전트 페이지
+
+
+
+router.get('/log', function(req, res, next) {
+  res.render('./main/Log/log' );
+}); // 평가문항 및 로그 추출 페이지
+
+
+// 그룹 페이지
+router.get('/group', function(req, res, next) {
+  res.render('./main/GroupPolicy/group' );
+}); // 그룹페이지 홈
+
+router.get('/newgroup', function(req, res, next) {
+  res.render('./main/GroupPolicy/NewGroup/newgroup' );
+});//신규 그룹페이지
+
+router.get('/oldgroup', function(req, res, next) {
+  res.render('./main/GroupPolicy/OldGroup/oldgroup' );
+});// 기존 그룹페이지
 
 
 
