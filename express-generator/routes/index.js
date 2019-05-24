54e116a4a1e91959010c64b17eec6a6066241f20
Choi YunJun <dbswnschl@gmail.com>
2019-05-24 16:35:14 +0900

    merge

--- express-generator/routes/index.js
+++ express-generator/routes/index.js
@@ -9,63 +9,79 @@ const DB = require('../db');
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
-router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard' );
+router.get('/dashboard', function (req, res, next) {
+  res.render('dashboard');
 });
 
 
-router.get('/404', function(req, res, next) {
-  res.render('./main/etc/404' );
+router.get('/404', function (req, res, next) {
+  res.render('./main/etc/404');
 });
 
 // User 기능과 관련된 페이지 시작
-router.get('/login', function(req, res, next) {
-  res.render('./main/User/login' );
+router.get('/login', function (req, res, next) {
+  res.render('./main/User/login');
 });
 
-router.get('/signup', function(req, res, next) {
-  res.render('./main/User/signup' );
+router.get('/signup', function (req, res, next) {
+  res.render('./main/User/signup');
 });
 
-router.get('/forgotpassword', function(req, res, next) {
-  res.render('./main/User/forgotpassword' );
+router.get('/forgotpassword', function (req, res, next) {
+  res.render('./main/User/forgotpassword');
 });
 
-router.get('/changeinfo', function(req, res, next) {
-  res.render('./main/User/changeinfo' );
+router.get('/changeinfo', function (req, res, next) {
+  res.render('./main/User/changeinfo');
 });
 
 // User 기능과 관련된 페이지 끝
-router.get('/agent', function(req, res, next) {
-  DB.get_agent_info().then(result=>{
-    res.render('./main/Agent/agent',result);
+router.get('/agent', function (req, res, next) {
+  DB.get_agent_info().then(result => {
+    console.log(result);
+    res.render('./main/Agent/agent', result);
   });
 });// 에이전트 페이지
-router.post('/add_manual_agent',(req,res,next)=>{
-  console.log(req.body);
-  DB.add_agent_info(req.body.txtIP,req.body.txtMac,req.body.txtOS,req.body.txtUseful,req.body.txtOwner,req.body.txtDesc).then(result=>{
-    console.log(result);
-    res.render('./main/Agent/agent');
+router.post('/add_manual_agent', (req, res, next) => {
+  let arr = [req.body.txtIP, req.body.txtMac, req.body.txtOS, req.body.txtUseful, req.body.txtOwner, req.body.txtDesc];
+  let isnull = false;
+  arr.forEach(element => {
+    if (element.length === 0) {
+      isnull = true;
+    }
   });
+  if (isnull) {
+    DB.get_agent_info().then(result => {
+        result['reason'] = 'error';
+      res.render('./main/Agent/agent', result);
+    });
+  } else {
+    DB.add_agent_info(req.body.txtIP, req.body.txtMac, req.body.txtOS, req.body.txtUseful, req.body.txtOwner, req.body.txtDesc).then(result => {
+      DB.get_agent_info().then(result2 => {
+        console.log(result2);
+        res.render('./main/Agent/agent', result2);
+      });
+    });
+  }
 });
 
 
-router.get('/log', function(req, res, next) {
-  res.render('./main/Log/log' );
+router.get('/log', function (req, res, next) {
+  res.render('./main/Log/log');
 }); // 평가문항 및 로그 추출 페이지
 
 
 // 그룹 페이지
-router.get('/group', function(req, res, next) {
-  res.render('./main/GroupPolicy/group' );
+router.get('/group', function (req, res, next) {
+  res.render('./main/GroupPolicy/group');
 }); // 그룹페이지 홈
 
-router.get('/newgroup', function(req, res, next) {
-  res.render('./main/GroupPolicy/NewGroup/newgroup' );
+router.get('/newgroup', function (req, res, next) {
+  res.render('./main/GroupPolicy/NewGroup/newgroup');
 });//신규 그룹페이지
 
-router.get('/oldgroup', function(req, res, next) {
-  res.render('./main/GroupPolicy/OldGroup/oldgroup' );
+router.get('/oldgroup', function (req, res, next) {
+  res.render('./main/GroupPolicy/OldGroup/oldgroup');
 });// 기존 그룹페이지
 
 
@@ -130,39 +146,39 @@ router.post('/delete_group_info', (req, res, next) => {
     res.render('test_mssql', { delete_group_info: JSON.stringify(err) });
   });
 });
-router.post('/put_xccdf', (req,res,next)=>{
-  DB.put_xccdf(req.body.xccdf_cd,req.body.file_name,req.body.file_path,req.body.inspect_os).then(result=>{
-    DB.view_tbl_xccdf().then(Result=>{
+router.post('/put_xccdf', (req, res, next) => {
+  DB.put_xccdf(req.body.xccdf_cd, req.body.file_name, req.body.file_path, req.body.inspect_os).then(result => {
+    DB.view_tbl_xccdf().then(Result => {
       res.render('test_mssql', { put_xccdf: JSON.stringify(Result.recordsets) });
-    }).catch(err=>{
+    }).catch(err => {
       res.render('test_mssql', { put_xccdf: JSON.stringify(err) });
     });
-  }).catch(err=>{
+  }).catch(err => {
     res.render('test_mssql', { put_xccdf: JSON.stringify(err) });
   });
 });
-router.post('/mapping_xccdf_group', (req,res,next)=>{
-  DB.mapping_xccdf_group(req.body.xccdf_cd,req.body.group_set_cd).then(result=>{
-    DB.view_tbl_xccdf_set_list().then(Result=>{
+router.post('/mapping_xccdf_group', (req, res, next) => {
+  DB.mapping_xccdf_group(req.body.xccdf_cd, req.body.group_set_cd).then(result => {
+    DB.view_tbl_xccdf_set_list().then(Result => {
       res.render('test_mssql', { mapping_xccdf_group: JSON.stringify(Result.recordsets) });
-    }).catch(err=>{
+    }).catch(err => {
       res.render('test_mssql', { mapping_xccdf_group: JSON.stringify(err) });
     });
-  }).catch(err=>{
+  }).catch(err => {
     res.render('test_mssql', { mapping_xccdf_group: JSON.stringify(err) });
   });
 });
-router.post('/add_group_info',(req,res,next)=>{
-  DB.add_group_info(req.body.name,req.body.create_time,req.body.active_time,req.body.agent_counting,req.body.inspection_period,req.body.discription)
-  .then(result=>{
-    DB.get_group_info().then(Result=>{
-      res.render('test_mssql',{add_group_info:JSON.stringify(Result.recordsets)});
-    }).catch(err=>{
+router.post('/add_group_info', (req, res, next) => {
+  DB.add_group_info(req.body.name, req.body.create_time, req.body.active_time, req.body.agent_counting, req.body.inspection_period, req.body.discription)
+    .then(result => {
+      DB.get_group_info().then(Result => {
+        res.render('test_mssql', { add_group_info: JSON.stringify(Result.recordsets) });
+      }).catch(err => {
+        res.render('test_mssql', { add_group_info: JSON.stringify(err) });
+      });
+    }).catch(err => {
       res.render('test_mssql', { add_group_info: JSON.stringify(err) });
     });
-  }).catch(err=>{
-    res.render('test_mssql', { add_group_info: JSON.stringify(err) });
-  });
 });
 
 router.get('/test', (req, res, next) => {
@@ -182,17 +198,17 @@ router.get('/view_tbl_group_info', (req, res, next) => {
     res.render('test_mssql', { group_info: JSON.stringify(err) });
   });
 });
-router.get('/get_new_xccdf_cd',(req,res,next)=>{
-  DB.get_new_xccdf_cd().then(result=>{
+router.get('/get_new_xccdf_cd', (req, res, next) => {
+  DB.get_new_xccdf_cd().then(result => {
     res.render('test_mssql', { get_new_xccdf_cd: JSON.stringify(result) });
-  }).catch(err=>{
+  }).catch(err => {
     res.render('test_mssql', { get_new_xccdf_cd: JSON.stringify(err) });
   });
 });
 ////////////////////////////////////////////////////////////////////
 
-router.get('/grouppolicy', function(req, res, next) {
-  res.render('grouppolicy' );
+router.get('/grouppolicy', function (req, res, next) {
+  res.render('grouppolicy');
 });
 
 
