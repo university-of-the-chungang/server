9bd871a1a86601f7089914111b3ed0532aa624a2
nakguan <nakguan@gmail.com>
2019-06-28 01:14:15 +0900

    apply group_delete & render change_group_v1.0

--- express-generator/routes/group/oldgroup.js
+++ express-generator/routes/group/oldgroup.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const DB = require('../../db');
 
 
-router.get('/', function (req, res, next) {
-  res.render('./main/GroupPolicy/OldGroup/oldgroup');
+router.post('/', function (req, res, next) {
+  if(req.body.change_group_NAME.length === 0) {
+    res.redirect('/group');
+  }else{
+    console.log(req.body.change_group_NAME);
+    DB.get_group_info(JSON.parse(req.body.change_group_NAME)).then(result => {
+      console.log(result);
+      res.render('./main/GroupPolicy/OldGroup/oldgroup', result);
+    })
+  }
 });// 기존 그룹페이지
-
 
 module.exports = router;
