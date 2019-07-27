a420c9add5815006193a2c64913a45958074ab7b
Choi YunJun <dbswnschl@gmail.com>
2019-07-27 12:30:15 +0900

    클릭시 결과확인

--- express-generator/routes/make_dashboard_html.js
+++ express-generator/routes/make_dashboard_html.js
@@ -1,7 +1,8 @@
 let fs = require('fs');
-let buildHtml = (row) => {
+exports.buildHtml = (row) => {
     return new Promise((resolve, reject) => {
         fs.readFile(__dirname + '/sample.html', 'utf-8', (err, html) => {
+            console.log(row);
             if (err) {
                 reject(err);
             }
@@ -9,7 +10,7 @@ let buildHtml = (row) => {
             html = html.replace(/\$\{un_obey_cnt\}/gi, un_obey_cnt);
             html = html.replace(/\$\{AGENT_IP\}/gi, row['IP']);
             html = html.replace(/\$\{AGENT_OS\}/gi, row['OS']);
-            html = html.replace(/\$\{rule_no\}/gi, row['INSPECT_CD'][0]);
+            html = html.replace(/\$\{rule_no\}/gi, row['INSPECT_CD']);
             html = html.replace(/\$\{pass_cnt\}/gi, row['GRD_SCORE']);
             html = html.replace(/\$\{fail_cnt\}/gi, un_obey_cnt);
             html = html.replace(/\$\{pass_percent\}/gi, row['GRD_SCORE'] / row['ITEM_CNT'] * 100);
@@ -23,6 +24,7 @@ let buildHtml = (row) => {
 
 }
 
+
 exports.make_html = (row_arr) => {
     let rows = row_arr;
     let paths = [];
@@ -32,7 +34,7 @@ exports.make_html = (row_arr) => {
             let filename = elem['CREATE_TIME'].split('T')[0] + '_' + elem['ACTIVE_TIME'].split('T')[0] + '_' + elem['GROUP_SET_CD'][0] + '_' + elem['IP'];
             let stream = fs.createWriteStream(__dirname + '/' + filename + ".html");
             stream.once('open', (fd) => {
-                buildHtml(elem).then(html => {
+                self.buildHtml(elem).then(html => {
                     stream.write(html);
                     stream.end();
                 })
